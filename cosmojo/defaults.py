2872228383159fca99c6040bf9e4bb422ab3968c
Giulio Fabbian <gfabbian@ias.u-psud.fr>
2018-04-23 21:26:28 +0200

    added deltazrei in param defaults

--- cosmojo/defaults.py
+++ cosmojo/defaults.py
@@ -18,14 +18,15 @@ default_cosmo_dict = {
     "r"      : 0.,     # tensor to scalar ratio at pivot scale
     "nt"     : None,   # tensor spectral index
     "ntrun"  : 0.,     # running of tensor spectral index
-    "pivot_scalar": 0.05, # pivot scale for scalar spectrum 
-    "pivot_tensor": 0.05, # pivot scale for tensor spectrum 
+    "pivot_scalar": 0.05, # pivot scale for scalar spectrum
+    "pivot_tensor": 0.05, # pivot scale for tensor spectrum
     "meffsterile" : 0.,   # effective mass of sterile neutrinos
     "neutrino_hierarchy": 'degenerate', # degenerate', 'normal', or 'inverted' (1 or 2 eigenstate approximation)
     "num_massive_neutrinos" : 1, # number of massive neutrinos (ignored unless hierarchy == 'degenerate')
     "standard_neutrino_neff": 3.046,
     "gamma0"     : 0.55,   # growth rate index
     "gammaa"     : 0.,     # growth rate index (series expansion term)
+    "deltazrei" : None,
     }
 
 default_cosmoCLASS_dict = {
@@ -46,7 +47,7 @@ default_cosmoCLASS_dict = {
     # "r"        : 0.,     # tensor to scalar ratio at pivot scale
     # "n_t"      : 'scc',   # tensor spectral index
     # "alpha_t"  : 'scc',     # running of tensor spectral index
-    "k_pivot": 0.05, # pivot scale for scalar spectrum 
+    "k_pivot": 0.05, # pivot scale for scalar spectrum
     # N_ur = 0.0
     # N_ncdm = 1
     # m_ncdm = 0.06
@@ -92,12 +93,12 @@ default_bao_survey_dict = {
 }
 
 default_limits = {
-    "limber_zmin" : 0.,   ### 
+    "limber_zmin" : 0.,   ###
     "limber_zmax" : None, ### If None, zstar (~1100) will be used
     "limber_lmin" : 2,
     "limber_lmax" : 2000,
     "limber_kmax" : 40.,
-    "dNdz_zmin"   : 0.,  
+    "dNdz_zmin"   : 0.,
     "dNdz_zmax"   : 5.,
     "lens_zmax"   : 5.,
     "pk_kmin"     : 1e-4, # 1/Mpc
@@ -105,13 +106,15 @@ default_limits = {
     "r_min"       : 1,    # Mpc
     "r_max"       : 300,  # Mpc
     "mass_min"    : -1,   #
-    "mass_max"    : -1,    # 
-    "halo_zmin"   : 5e-3,   ### 
+    "mass_max"    : -1,    #
+    "halo_zmin"   : 5e-3,   ###
     "halo_zmax"   : 10, ### If None, zstar (~1100) will be used
     "halo_lmin"   : 2,
     "halo_lmax"   : 2000,
+#    "halo_Mmin"   : 5e5,  # M_sun
+#    "halo_Mmax"   : 5e15, # M_sun
     "halo_Mmin"   : 5e5,  # M_sun
-    "halo_Mmax"   : 5e15, # M_sun
+    "halo_Mmax"   : 5e20, # M_sun
 }
 
 default_precision = {
@@ -125,8 +128,8 @@ default_precision = {
     "lens_precision": 1.48e-6,
     "corr_precision": 1e-5,
     "global_precision": 1.48e-32, ### Since the code has large range of values
-                                  ### from say 1e-10 to 1e10 we don't want to 
-                                  ### use absolute tolerances, instead using 
+                                  ### from say 1e-10 to 1e10 we don't want to
+                                  ### use absolute tolerances, instead using
                                   ### relative tolerances to define convergence
                                   ### of our integrands
     "divmax":20,                   ### Maximum number of subdivisions for
@@ -166,7 +169,7 @@ default_halo_dict = {
 #     "halo_limit" : 100,
 #     "kernel_npoints": 50,
 #     "kernel_precision": 1.48e-6,
-#     "kernel_limit": 100, ### If the variable force_quad is set in the Kernel 
+#     "kernel_limit": 100, ### If the variable force_quad is set in the Kernel
 #                          ### class this value sets the limit for the quad
 #                          ### integration
 #     "kernel_bessel_limit": 8, ### Defines how many zeros before cutting off
@@ -176,8 +179,8 @@ default_halo_dict = {
 #     "window_npoints": 100,
 #     "window_precision": 1.48e-6,
 #     "global_precision": 1.48e-32, ### Since the code has large range of values
-#                                   ### from say 1e-10 to 1e10 we don't want to 
-#                                   ### use absolute tolerances, instead using 
+#                                   ### from say 1e-10 to 1e10 we don't want to
+#                                   ### use absolute tolerances, instead using
 #                                   ### relative tolerances to define convergence
 #                                   ### of our integrands
 #     "divmax":20                   ### Maximum number of subdivisions for
